e8ecaa1e5e1d633c02a793ffc64e51516e196c38
hoseungzzang <wnghtmd99@naver.com>
2023-02-27 13:40:38 +0900

    update(reservation) : 권한 추가

--- src/apis/reservation/reservationApis.js
+++ src/apis/reservation/reservationApis.js
@@ -1,27 +1,28 @@
 import axios from "axios";
+import authAxios from "@/apis/common/authAxios";
 
 
 const domain = 'http://' + window.location.hostname + ( window.location.hostname.indexOf('armysseung.iptime.org') == -1 ? ':8080' : ':3258')
 
 export const getReservaiontListApi = async () => {
-  const res = await axios.get(`${domain}/api/reservation/list`)
+  const res = await authAxios.get(`${domain}/api/reservation/list`)
 
   return res.data
 }
 
 export const getReservaiontDetailApi = async (orNo) => {
-  const res = await axios.get(`${domain}/api/reservation/detail/products/${orNo}`)
+  const res = await authAxios.get(`${domain}/api/reservation/detail/products/${orNo}`)
 
   return res.data
 }
 
 export const modifyReservationCheckApi = async (noList) => {
-  const res = await axios.put(`${domain}/api/reservation/detail/modify`,noList)
+  const res = await authAxios.put(`${domain}/api/reservation/detail/modify`,noList)
   return res.data
 }
 
 export const modifyReservationCancleApi = async (noList) => {
-  const res = await axios.put(`${domain}/api/reservation/detail/cancle`,noList)
+  const res = await authAxios.put(`${domain}/api/reservation/detail/cancle`,noList)
   return res.data
 }
 
